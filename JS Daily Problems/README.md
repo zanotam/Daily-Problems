c17018eaa3d80cc8d9cf4ba166cb3d3c3475f34e
Robert Baker <zanotam@gmail.com>
2021-07-06 14:31:50 -0700

    Solved leetcode1338 in JS

---
+++ JS Daily Problems/README.md
@@ -0,0 +1,3 @@
+# JS Daily Problems
+
+
---
+++ JS Daily Problems/leetcode1338.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function (arr) {
+    /*
+     create a map of number to count of number
+     sort map by count
+     want sum_i of count_i starting from 0 to n
+     of sorted map to be greater than or equal to arr.size/2
+     n is the return. 
+     */
+    let minSum = arr.length / 2;
+    let counts = arr.reduce(function (counter, item) {
+        counter[item] = counter.hasOwnProperty(item) ? counter[item] + 1 : 1;
+        return counter;
+    }, {})
+    let unsorted = [];
+    for (let key in counts) {
+        unsorted.push(`${counts[key]}: ${key}`);
+    }
+    let sorted = unsorted.sort((a, b) => { return b.split(':')[0] - a.split(':')[0] });
+    let sum = 0;
+    let index = -1;
+    while (sum < minSum) {
+        index++;
+        sum += parseInt(sorted[index].split(':')[0]);
+    }
+    return index + 1;
+};
